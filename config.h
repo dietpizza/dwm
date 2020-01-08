06547b0327aad4de33a482826f13ba272ea79a8d
Rohan Biswas <brainhacker550@gmail.com>
2020-01-08 10:33:30 +0530

    updated colors

--- config.h
+++ config.h
@@ -1,23 +1,23 @@
 /* appearance */
-static const unsigned int borderpx  = 2;
+static const unsigned int borderpx  = 1;
 static const unsigned int snap      = 32;
 static const unsigned int gappih    = 10;
 static const unsigned int gappiv    = 10;
 static const unsigned int gappoh    = 10;
 static const unsigned int gappov    = 10;
-static const int smartgaps          = 0;
+static const int smartgaps          = 1;
 static const int showbar            = 1; 
 static const int topbar             = 1; 
 static const int horizpadbar        = 1;
 static const int vertpadbar         = 1;
-static const unsigned int baralpha = 0xc0;
-static const unsigned int borderalpha = 0xc0;
+static const unsigned int baralpha = 0xdd;
+static const unsigned int borderalpha = 0xdd;
 static const char *fonts[]          = { "Terminus:size=9" };
 static const char col_gray1[]       = "#002b36";
 static const char col_gray2[]       = "#073642";
 static const char col_gray3[]       = "#93a1a1";
 static const char col_gray4[]       = "#eee8d5";
-static const char col_cyan[]        = "#268bd2";
+static const char col_cyan[]        = "#586e75";
 static const char *colors[][3]      = {
 /*                                                       fg         bg         border   */
                                           [SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
@@ -83,7 +83,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[]    = { "dmenu_run", NULL };
 static const char *termcmd[]     = { "st", NULL };
 static const char *ranger[]      = { "st", "-e", "ranger" , NULL };
-static const char *chromium[]    = { "chromium","--force-dark-mode",  NULL };
+static const char *qutebrowser[] = { "qutebrowser",  NULL };
 static const char *nemo[]        = { "nemo", NULL };
 static const char *scrot[]       = { "scrot", NULL };
 static const char *codium[]      = { "codium", NULL };
@@ -96,7 +96,7 @@ static Key keys[] = {
     { MODKEY,                       XK_l,       spawn,          {.v = dmenucmd } },
     { MODKEY,                       XK_p,       spawn,          {.v = power } },
     { MODKEY,                       XK_Return,  spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_b,       spawn,          {.v = chromium} },
+    { MODKEY,                       XK_b,       spawn,          {.v = qutebrowser } },
     { MODKEY,                       XK_f,       spawn,          {.v = nemo } },
     { MODKEY,                       XK_k,       spawn,          {.v = codium } },
     { MODKEY,                       XK_o,       spawn,          {.v = ranger } },
